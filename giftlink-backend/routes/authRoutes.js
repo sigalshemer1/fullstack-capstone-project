8738e5cd429afc7d41a07c4b6ff71ec98964cf06
sigalshemer1 <sigal.shemer68@gmail.com>
2024-08-03 19:18:16 +0100

    Update authRoutes.js

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -7,26 +7,21 @@ const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
-
 const logger = pino();  // Create a Pino logger instance
+dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
-
 router.post('/register', async (req, res) => {
     try {
         // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
         const db = await connectToDatabase();
-
         // Task 2: Access MongoDB collection
         const collection = db.collection("users");
-
         //Task 3: Check for existing email
         const existingEmail = await collection.findOne({ email: req.body.email });
-
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
-
-        // {{insert code here}} //Task 4: Save user details in database
+        //Task 4: Save user details in database
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -34,19 +29,16 @@ router.post('/register', async (req, res) => {
             password: hash,
             createdAt: new Date(),
         });
-
-         // {{insert code here}} //Task 5: Create JWT authentication with user._id as payload
-         const payload = {
+        const payload = {
             user: {
                 id: newUser.insertedId,
             },
         };
-
         const authtoken = jwt.sign(payload, JWT_SECRET);
-        
         logger.info('User registered successfully');
         res.json({authtoken,email});
     } catch (e) {
          return res.status(500).send('Internal server error');
     }
 });
+module.exports = router;
