8cb2959f79b14c6c10514a3c34aae8106238d2d8
sigalshemer1 <sigal.shemer68@gmail.com>
2024-08-07 14:30:20 +0100

    Update app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -4,19 +4,16 @@ const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
-const { loadData } = require("./util/import-mongo/index");
+const {loadData} = require("./util/import-mongo/index");
 const app = express();
-app.use("*", cors());
+app.use("*",cors());
 const port = 3060;
-
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
     .catch((e) => console.error('Failed to connect to DB', e));
-
 app.use(express.json());
-
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
 const authRoutes = require('./routes/authRoutes');
@@ -24,22 +21,18 @@ const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 app.use(pinoHttp({ logger }));
-
 // Use Routes
 app.use('/api/gifts', giftRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
-
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
-
-app.get("/", (req, res) => {
+app.get("/",(req,res)=>{
     res.send("Inside the server");
 });
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
